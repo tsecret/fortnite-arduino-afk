694318346f07c3a6ed2eb8504dcca367ba8309e6
tsecret <timichfull@gmail.com>
2025-01-23 21:56:49 +0500

    working script

--- src/main.py
+++ src/main.py
@@ -2,24 +2,12 @@ from gamemode.tttycoon import TikTokTycoon
 from gamemode.battle_royale import BattleRoyale
 from gamemode.lego import Lego
 
-from doctr.io import DocumentFile
-from doctr.models import ocr_predictor
-
 def main():
 
-  model = ocr_predictor(pretrained=True)
-  doc = DocumentFile.from_images("./xp.png")
+  while True:
 
-  result = model(doc)
-  print(result.pages[0].blocks[0])
-
-  # tycoon = Lego()
-  # tycoon.checkExp()
-
-  # while True:
-
-    # tycoon = TikTokTycoon()
-    # tycoon.start()
+    tycoon = TikTokTycoon()
+    tycoon.start()
 
     # tycoon = TikTokTycoon()
     # tycoon.start()
